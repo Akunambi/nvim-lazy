6a2803b9a46d9e3784de5a0dc0f3f26e28dd5f3c
akunambi <akunambi65@gmail.com>
2025-05-18 17:51:57 +0300

    remap: oc2 yanking

--- lua/plugins/oc2.lua
+++ lua/plugins/oc2.lua
@@ -6,7 +6,7 @@ return {
       vim.keymap.set("n", "<leader>c", function()
         require("osc52").copy_operator()
       end, { expr = true })
-      vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
+      vim.keymap.set("n", "<leader>yy", "<leader>y_", { remap = true })
     end,
   },
 }
